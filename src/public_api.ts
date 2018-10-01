ae11e37f88a8faf320a4235f5ad676798d5c576b
Melissa van Dijk <melissa.vandijk@peterconnects.com>
2018-10-01 10:05:48 +0200

    <module>: <message (under 50 chars)
    <description>
    
    Story #<story number>
    Task #<task number>

--- src/public_api.ts
+++ src/public_api.ts
@@ -1,9 +1,6 @@
 /*
  * Public API Surface of pwa
  */
-
-//export * from './lib/pwa.service';
-//export * from './lib/pwa.component';
 export * from './pwa.module';
 export * from './components/widgetbase.component';
 export * from './models/call';
